dda0aca9f769287675d943e83c3fe29bbf0b21b5
MagicGopher <gopher997@gmail.com>
2025-01-25 15:07:42 +0800

    feat(data type): numeric type example
    add date type numeric type example

---
+++ 01_base/02_datatypes/demo01/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// go数据类型有数值型、浮点型、布尔型、复数型、字节型、字符串型
+
+func main() {
+	// 数值类型
+	fmt.Println("数值类型-无符号位")
+	// 无符号整数位
+	var n1 uint = 100
+	var n2 uint8 = 255
+	var n3 uint16 = 65535
+	var n4 uint32 = 4294967295
+	var n5 uint64 = 18446744073709551615
+	fmt.Println("uint类型变量n1的值是:", n1)
+	fmt.Println("uint8类型变量n2的值是:", n2)
+	fmt.Println("uint16类型变量n3的值是:", n3)
+	fmt.Println("uint32类型变量n4的值是:", n4)
+	fmt.Println("uint64类型变量n5的值是:", n5)
+	fmt.Println("============")
+	fmt.Println("数值类型-无符号位")
+	// 有符号整数位
+	var n6 int = -100
+	var n7 int8 = -128
+	var n8 int16 = -32768
+	var n9 int32 = -2147483648
+	var n10 int64 = -9223372036854775808
+	fmt.Println("int类型变量n6的值是:", n6)
+	fmt.Println("int8类型变量n7的值是:", n7)
+	fmt.Println("int16类型变量n8的值是:", n8)
+	fmt.Println("int32类型变量n9的值是:", n9)
+	fmt.Println("int64类型变量n10的值是:", n10)
+}
