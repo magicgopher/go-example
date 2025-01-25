1442110474c5e6dc3eeb1dde6ec8fdf62a45bce8
MagicGopher <gopher997@gmail.com>
2025-01-25 15:38:36 +0800

    feat(data types): data types example
    add int,int8,int16,int32,int64 type example
    add uint,uint8,uint16,uint32,uint64 type example
    add float64,float128 example
    add bool type example
    add byte,rune,string type example

---
+++ 01_base/02_datatypes/demo02/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 数值型（浮点型）
+	var f1 float32 = 3.14159265358979
+	var f2 float64 = 3.14159265358979323846264338327950288
+
+	fmt.Println(f1)
+	fmt.Println(f2)
+}
---
+++ 01_base/02_datatypes/demo05/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 字符
+	var a byte = 'A' // 等同于 uint8
+	var b rune = '本' // 等同于 int32
+	fmt.Printf("%c %c\n", a, b)
+
+	// 字符串
+	var s string = "Hello, World!"
+	fmt.Println(s)
+}
